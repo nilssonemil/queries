426dc2102173d0ecec0d3a81554041539e31830e
Emil Nilsson <emil.nilsson@protonmail.com>
2023-03-20 22:43:34 +0100

    Find all users using ldap search

--- src/main/scala/solutions/shitops/query/app/Main.scala
+++ src/main/scala/solutions/shitops/query/app/Main.scala
@@ -4,6 +4,7 @@ import cats.effect.IO
 import io.circe.generic.auto._
 import org.http4s.HttpRoutes
 import cats.effect._
+import cats.implicits.toSemigroupKOps
 import org.http4s.circe.CirceEntityCodec.circeEntityEncoder
 import org.http4s.dsl.io._
 import com.comcast.ip4s._
@@ -12,7 +13,7 @@ import doobie.util.ExecutionContexts
 import org.http4s.ember.server._
 import org.http4s.implicits._
 import org.http4s.server.Router
-import solutions.shitops.query.infrastructure.QuestionRepository
+import solutions.shitops.query.infrastructure.{QuestionRepository, UserRepository}
 import doobie.implicits._
 
 case class Question(title: String)
@@ -42,12 +43,20 @@ object Main extends IOApp {
     )
   } yield xa
 
+  private def userService() = HttpRoutes.of[IO] {
+    case GET -> Root / "users" =>
+      Ok(UserRepository.getUsers)
+  }
+
 
   private def questionService(transactor: HikariTransactor[IO]) = HttpRoutes.of[IO] {
     case GET -> Root / "questions" => {
       QuestionRepository.getQuestions.transact(transactor).flatMap(Ok(_))
     }
   }
+  private def services(transactor: HikariTransactor[IO]) =
+    questionService(transactor) <+> userService()
+
   private def httpApp(transactor: HikariTransactor[IO]) =
-    Router("/" -> questionService(transactor)).orNotFound
+    Router("/" -> services(transactor)).orNotFound
 }
