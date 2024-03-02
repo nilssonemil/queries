14f4442c86a1699ab8655411e3e08d3725c34f3b
Emil Nilsson <emil.nilsson@protonmail.com>
2024-03-02 21:24:47 +0100

    chore: add more test data

--- sql/002-data.sql
+++ sql/002-data.sql
@@ -2,7 +2,31 @@ INSERT INTO users VALUES
   ('tom.hanks', 'http://localhost:5000/images/cat-avatar-2.jpg'),
   ('marie.curie', 'http://localhost:5000/images/cat-avatar-1.jpg'),
   ('anders.antonsen', 'http://localhost:5000/images/cat-avatar-3.jpg'),
-  ('salvador.dali', 'http://localhost:5000/images/cat-avatar-4.jpg');
+  ('salvador.dali', 'http://localhost:5000/images/cat-avatar-4.jpg'),
+  ('john.doe', 'http://localhost:5000/images/cat-avatar-1.jpg'),
+  ('alice.wonderland', 'http://localhost:5000/images/cat-avatar-2.jpg'),
+  ('clark.kent', 'http://localhost:5000/images/cat-avatar-3.jpg'),
+  ('bruce.wayne', 'http://localhost:5000/images/cat-avatar-4.jpg'),
+  ('lara.croft', NULL),
+  ('indiana.jones', NULL),
+  ('walter.white', 'http://localhost:5000/images/cat-avatar-1.jpg'),
+  ('daenerys.targaryen', 'http://localhost:5000/images/cat-avatar-2.jpg'),
+  ('sherlock.holmes', 'http://localhost:5000/images/cat-avatar-3.jpg'),
+  ('james.bond', 'http://localhost:5000/images/cat-avatar-4.jpg'),
+  ('elizabeth.bennet', NULL),
+  ('tony.stark', 'http://localhost:5000/images/cat-avatar-1.jpg'),
+  ('harry.potter', 'http://localhost:5000/images/cat-avatar-2.jpg'),
+  ('hermione.granger', 'http://localhost:5000/images/cat-avatar-3.jpg'),
+  ('frodo.baggins', 'http://localhost:5000/images/cat-avatar-4.jpg'),
+  ('aragorn', 'http://localhost:5000/images/cat-avatar-1.jpg'),
+  ('luke.skywalker', NULL),
+  ('leia.organa', 'http://localhost:5000/images/cat-avatar-1.jpg'),
+  ('samwise.gamgee', 'http://localhost:5000/images/cat-avatar-2.jpg'),
+  ('han.solo', 'http://localhost:5000/images/cat-avatar-3.jpg'),
+  ('captain.kirk', 'http://localhost:5000/images/cat-avatar-4.jpg'),
+  ('spock', NULL),
+  ('luffy.monkey', 'http://localhost:5000/images/cat-avatar-1.jpg'),
+  ('naruto.uzumaki', 'http://localhost:5000/images/cat-avatar-2.jpg');
 
 INSERT INTO questions VALUES
   (
@@ -16,6 +40,126 @@ INSERT INTO questions VALUES
     'marie.curie',
     'How does the horse move?',
     'If Magnus does not know how are we supposed to?'
+  ),
+  (
+    '1e8c901f-8654-4bf0-8d38-32f1a1a8cb31',
+    'john.doe',
+    'How can I improve my programming skills?',
+    'I want to become a better programmer. What are some tips and resources I can use to improve my programming skills?'
+  ),
+  (
+    '4f2f33d2-649c-4c47-bb2d-1f1c36d1d3c2',
+    'alice.wonderland',
+    'What are some interesting places to visit in Wonderland?',
+    'I''m planning a trip to Wonderland. Can anyone recommend some must-visit places or attractions there?'
+  ),
+  (
+    '8ef0f128-7321-4cbf-8f14-d71f92ebe9ec',
+    'clark.kent',
+    'How can I keep my secret identity safe?',
+    'As a superhero with a secret identity, I''m concerned about keeping it safe. What are some strategies or precautions I can take to protect my secret identity?'
+  ),
+  (
+    'ac60d5f4-6182-4e9c-8b6e-0b67312d7a56',
+    'bruce.wayne',
+    'What are the benefits of being a billionaire playboy?',
+    'Being a billionaire playboy seems like a glamorous lifestyle, but what are the actual benefits?'
+  ),
+  (
+    'fbc7c317-b9f0-4d1f-9f59-41ac27af7d82',
+    'lara.croft',
+    'How can I survive in dangerous environments?',
+    'As an adventurer, I often find myself in dangerous environments. What are some survival tips for navigating these situations?'
+  ),
+  (
+    '30367a41-81bc-4952-bbb2-2f16fbdcf8b3',
+    'indiana.jones',
+    'What are the most valuable artifacts to find?',
+    'As an archaeologist, I''m always on the lookout for valuable artifacts. What are some of the most sought-after or valuable artifacts to find?'
+  ),
+  (
+    '9b3e2f67-ef0a-4e71-bf41-26d4a76b344a',
+    'walter.white',
+    'How can I start my own meth empire?',
+    'I''m considering entering the drug trade and starting my own meth empire. What are the steps I should take to get started?'
+  ),
+  (
+    '5cd38c24-2077-4e56-9fd5-1d156f75817f',
+    'daenerys.targaryen',
+    'How can I train dragons?',
+    'I want to become a dragon rider like my ancestors. What are some tips for training and riding dragons?'
+  ),
+  (
+    '3729482f-7efb-4d78-9a8c-61b48be3f9d2',
+    'sherlock.holmes',
+    'What are some effective deduction techniques?',
+    'As a detective, I rely on deductive reasoning to solve cases. What are some effective techniques for making deductions?'
+  ),
+  (
+    'fa2c9b58-ade2-4e5f-b40e-17c1129f8b69',
+    'james.bond',
+    'How can I become a spy?',
+    'I''m interested in becoming a spy. What are the steps I should take to pursue a career in espionage?'
+  ),
+  (
+    'f9c7e34a-8b43-45a8-a3b6-d34f176e9c18',
+    'elizabeth.bennet',
+    'What are some qualities of a good husband?',
+    'As a young woman, I''m curious about what qualities I should look for in a potential husband.'
+  ),
+  (
+    '3e74d3a8-5d77-4fd1-931c-6b12abfa0e3a',
+    'tony.stark',
+    'How can I build a suit of powered armor?',
+    'I''m interested in building a suit of powered armor like Iron Man. What are the technical challenges and considerations involved?'
+  ),
+  (
+    '7b8c9e2d-8f1a-4e9e-ba4a-9257a5ac4db9',
+    'harry.potter',
+    'What are some advanced spells?',
+    'As a wizard, I''m interested in learning more advanced spells beyond the standard curriculum.'
+  ),
+  (
+    '7233723a-8b8d-4f08-a932-baae227293f8',
+    'hermione.granger',
+    'How can I master transfiguration magic?',
+    'I want to become proficient in transfiguration magic. What are some tips for mastering this branch of magic?'
+  ),
+  (
+    '6fc76b18-dbd3-4ee2-af7f-0fe9a1a153b7',
+    'frodo.baggins',
+    'How can I destroy the One Ring?',
+    'I''ve been entrusted with the task of destroying the One Ring. What are some strategies for accomplishing this mission?'
+  ),
+  (
+    'b1958c13-392f-44c5-80d7-6e5e9d98d4a2',
+    'aragorn',
+    'How can I reclaim the throne of Gondor?',
+    'As the rightful heir to the throne of Gondor, I seek to reclaim my birthright. What are the steps I should take to achieve this goal?'
+  ),
+  (
+    '2be74cb4-c296-495a-bd51-6b5f32b26f99',
+    'luke.skywalker',
+    'How can I become a Jedi Knight?',
+    'I want to become a Jedi Knight like my father before me. What are the steps I should take to begin my training?'
+  ),
+  (
+    '18ad5e32-32b4-437d-bec1-9b1f1f3a7aaf',
+    'leia.organa',
+    'How can I lead the Rebel Alliance to victory?',
+    'As a leader of the Rebel Alliance, I seek strategies for defeating the Empire and restoring freedom to the galaxy.'
+  ),
+  (
+    '1a4dc3f7-e1a2-4db5-90a1-6fbdc1f8a3f2',
+    'samwise.gamgee',
+    'How can I support Frodo on his quest?',
+    'As Frodo''s loyal friend and companion, I want to know how I can best support him on his quest to destroy the One Ring.'
+  ),
+  (
+    'd0c12ed8-70cb-4d7d-9ad0-3194c37c1d2e',
+    'spock',
+    'What are the logical fallacies to avoid?',
+    'As a Vulcan, I value logic above all else. What are some common logical fallacies to be aware of and avoid?'
   );
 
 INSERT INTO answers VALUES
@@ -39,4 +183,144 @@ INSERT INTO answers VALUES
     'anders.antonsen',
     'no, forwards!!!',
     '2024-02-06 13:37:37'
+  ),
+  (
+    'a1b90265-f5cc-482f-9b7d-46f7f24fde01',
+    '1e8c901f-8654-4bf0-8d38-32f1a1a8cb31',
+    'tony.stark',
+    'To become a better programmer, I recommend practicing regularly, working on projects that interest you, and seeking feedback from experienced developers.',
+    '2024-03-01 10:00:00'
+  ),
+  (
+    '40a28234-6cb3-4a5d-8d69-d1a19b31d485',
+    '4f2f33d2-649c-4c47-bb2d-1f1c36d1d3c2',
+    'harry.potter',
+    'Some interesting places to visit in Wonderland include the Mad Hatter''s tea party, the Queen of Hearts'' castle, and the Cheshire Cat''s forest.',
+    '2024-03-02 11:00:00'
+  ),
+  (
+    '2f946fe7-5276-4a52-a3cf-9517683e9c25',
+    '8ef0f128-7321-4cbf-8f14-d71f92ebe9ec',
+    'hermione.granger',
+    'To keep your secret identity safe, it''s important to maintain a low profile, use disguises when necessary, and be cautious about who you trust with your secret.',
+    '2024-03-03 12:00:00'
+  ),
+  (
+    'd3aa0845-7b45-4e02-ba11-d343f28af1c6',
+    '7233723a-8b8d-4f08-a932-baae227293f8',
+    'frodo.baggins',
+    'The benefits of being a billionaire playboy include access to luxury, influence in high society, and the ability to fund philanthropic endeavors.',
+    '2024-03-04 13:00:00'
+  ),
+  (
+    'a6f645bb-8eb3-4915-9d9d-7c2a90d16fe9',
+    'fbc7c317-b9f0-4d1f-9f59-41ac27af7d82',
+    'luke.skywalker',
+    'To survive in dangerous environments, it''s essential to be prepared with proper equipment, knowledge of survival techniques, and a strong mindset.',
+    '2024-03-05 14:00:00'
+  ),
+  (
+    '34c5b75e-ee19-4e64-8932-97b6403623fb',
+    '30367a41-81bc-4952-bbb2-2f16fbdcf8b3',
+    'leia.organa',
+    'The most valuable artifacts to find include ancient relics, lost treasures of civilizations, and artifacts with magical or mystical properties.',
+    '2024-03-06 15:00:00'
+  ),
+  (
+    '6b65f44a-6a5e-41d0-b36a-35c1c3d9fc6d',
+    '2be74cb4-c296-495a-bd51-6b5f32b26f99',
+    'naruto.uzumaki',
+    'To start your own meth empire, you''ll need to establish a distribution network, manufacture high-quality product, and protect your territory from rival gangs and law enforcement.',
+    '2024-03-07 16:00:00'
+  ),
+  (
+    '4371a0c5-7295-45fc-b0d2-79be46e5b13a',
+    '18ad5e32-32b4-437d-bec1-9b1f1f3a7aaf',
+    'luffy.monkey',
+    'To train dragons, you''ll need patience, trust, and an understanding of their behavior and instincts. Start with bonding exercises and gradually introduce commands and training routines.',
+    '2024-03-08 17:00:00'
+  ),
+  (
+    '3b8a2b68-d027-44a4-8d4a-399ded8c3338',
+    '3729482f-7efb-4d78-9a8c-61b48be3f9d2',
+    'spock',
+    'Effective deduction techniques include observation, logical reasoning, and inference based on evidence. Avoid jumping to conclusions without sufficient evidence.',
+    '2024-03-09 18:00:00'
+  ),
+  (
+    'dbab3e94-7613-492a-987f-0110ed45ccbc',
+    'd0c12ed8-70cb-4d7d-9ad0-3194c37c1d2e',
+    'captain.kirk',
+    'To become a spy, you''ll need to develop skills in surveillance, espionage, and covert operations. Start by studying intelligence agencies and building a network of contacts.',
+    '2024-03-10 19:00:00'
+  ),
+  (
+    'e162cb97-2c9f-4874-9ac5-fb73599d9a36',
+    '1e8c901f-8654-4bf0-8d38-32f1a1a8cb31',
+    'bruce.wayne',
+    'Improving programming skills requires dedication, continuous learning, and practical application. Consider working on personal projects, participating in coding challenges, and seeking feedback from peers.',
+    '2024-03-11 20:00:00'
+  ),
+  (
+    '4fc3822f-efdc-45b0-8b80-07e8c1911839',
+    '4f2f33d2-649c-4c47-bb2d-1f1c36d1d3c2',
+    'lara.croft',
+    'Wonderland is full of wonders, including the mystical forest of Jabberwocky, the whimsical tea gardens of the White Rabbit, and the enchanting halls of the Red Queen.',
+    '2024-03-12 21:00:00'
+  ),
+  (
+    'a2bba31b-fd61-43d0-b3a6-1019b60dd192',
+    '8ef0f128-7321-4cbf-8f14-d71f92ebe9ec',
+    'indiana.jones',
+    'To protect your secret identity, maintain a separate persona, use disguises when necessary, and be cautious about revealing personal information to others.',
+    '2024-03-13 22:00:00'
+  ),
+  (
+    'b249fe0c-91d0-4ff9-89d2-707c3a7d55e1',
+    '7233723a-8b8d-4f08-a932-baae227293f8',
+    'walter.white',
+    'Being a billionaire playboy allows for extravagant living, access to exclusive events, and the ability to support charitable causes.',
+    '2024-03-14 23:00:00'
+  ),
+  (
+    '2cc6933f-bb4a-4b1c-a7d1-005e16017c84',
+    'fbc7c317-b9f0-4d1f-9f59-41ac27af7d82',
+    'daenerys.targaryen',
+    'Surviving in dangerous environments requires resourcefulness, adaptability, and physical fitness. Equip yourself with survival gear and knowledge of wilderness survival techniques.',
+    '2024-03-15 00:00:00'
+  ),
+  (
+    '3477d484-6848-4d3b-9f76-dc52a1ae8ff4',
+    '30367a41-81bc-4952-bbb2-2f16fbdcf8b3',
+    'sherlock.holmes',
+    'Valuable artifacts include historical relics, precious gems, and artifacts with cultural or archaeological significance.',
+    '2024-03-16 01:00:00'
+  ),
+  (
+    'd58383b4-4e50-4641-bda5-71f73c1618e5',
+    '2be74cb4-c296-495a-bd51-6b5f32b26f99',
+    'james.bond',
+    'To train dragons, establish trust through feeding and care, practice commands and flight techniques, and reinforce positive behavior with rewards.',
+    '2024-03-17 02:00:00'
+  ),
+  (
+    '3b1af6d9-bc53-45dc-b3a2-8c4d23e174d2',
+    '18ad5e32-32b4-437d-bec1-9b1f1f3a7aaf',
+    'elizabeth.bennet',
+    'To lead the Rebel Alliance, inspire hope, build alliances, and strategically weaken the Empire through targeted strikes and guerrilla warfare tactics.',
+    '2024-03-18 03:00:00'
+  ),
+  (
+    '097a2958-1a20-4b1b-9db3-e4317ff7b107',
+    '3729482f-7efb-4d78-9a8c-61b48be3f9d2',
+    'samwise.gamgee',
+    'Support Frodo by providing emotional encouragement, carrying the burden when needed, and remaining loyal and steadfast in the face of adversity.',
+    '2024-03-19 04:00:00'
+  ),
+  (
+    'aa3f336b-e1ff-491b-92bb-b7595707b4b5',
+    'd0c12ed8-70cb-4d7d-9ad0-3194c37c1d2e',
+    'spock',
+    'Logical fallacies to avoid include ad hominem attacks, straw man arguments, and false dichotomies. Focus on evidence-based reasoning and logical consistency.',
+    '2024-03-20 05:00:00'
   );
